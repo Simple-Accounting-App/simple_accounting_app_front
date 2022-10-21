6ccf4254205c6d06924699f3f7a72783f56864f3
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-21 12:37:03 -0400

    feat(App.js): basic home skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Simple Accounting App</h2>
+      <h2>Current user: Astérix</h2>
+      <br/>
+      <h3>Accounts</h3>
+      <br/>
+      <h3>Transactions</h3>
     </div>
   );
 }
