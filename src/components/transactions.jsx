86a1e4a124a99089beac5e52280e317f454d71a9
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-23 13:57:03 -0400

    feat(transactions.jsx): add design to transactions table

--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -1,4 +1,5 @@
 import { useQuery, gql } from '@apollo/client';
+import { Table } from 'antd';
 
 const GET_TRANSACTIONS = gql`
   {
@@ -31,23 +32,40 @@ export default function Transactions() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
+  const columns = [
+    {
+      title: 'Debit account',
+      dataIndex: 'debitAccount',
+      key: 'debitAccount',
+    },
+    {
+      title: 'Credit account',
+      dataIndex: 'debitAccount',
+      key: 'debitAccount',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+    },
+    {
+      title: 'Transfer Date',
+      dataIndex: 'transferDate',
+      key: 'transferDate',
+    },
+  ];
+
+  let dataSource = data.currentUserTransactions.map(({ amount, transferDate, debitAccount, creditAccount }) => (
+    {
+      debitAccount: debitAccount.user.name + "'s " + debitAccount.name,
+      creditAccount: creditAccount.user.name + "'s " + creditAccount.name,
+      transferDate: formatDate(transferDate),
+      amount: amount
+    }
+  ));
+
   return( 
-    <table>
-      <tr>
-        <th>Debit account</th>
-        <th>Credit account</th>
-        <th>Transfer date</th>
-        <th>Amount</th>
-      </tr>
-      {data.currentUserTransactions.map(({ amount, transferDate, debitAccount, creditAccount }) => (
-          <tr>
-            <td>{debitAccount.user.name}'s {debitAccount.name}</td>
-            <td>{creditAccount.user.name}'s {creditAccount.name}</td>
-            <td>{formatDate(transferDate)}</td>
-            <td>{amount}</td>
-          </tr>
-      ))}
-    </table>
+    <Table dataSource={dataSource} columns={columns}/>
   );
 }
 
