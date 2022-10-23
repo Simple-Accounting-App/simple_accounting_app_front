d36a2ef0f2e037e861f33cd1558ad302e8633721
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-23 12:32:28 -0400

    lint(components): align indent across all components

--- src/components/accounts.jsx
+++ src/components/accounts.jsx
@@ -1,33 +1,32 @@
 import { useQuery, gql } from '@apollo/client';
 
 const GET_ACCOUNTS = gql`
-    {
-        currentUserAccounts {
-        name
-        currentAmount
-        }
+  {
+    currentUserAccounts {
+      name
+      currentAmount
     }
-  
+  }
 `;
 
 export default function Accounts() {        
-    const { loading, error, data } = useQuery(GET_ACCOUNTS);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+  const { loading, error, data } = useQuery(GET_ACCOUNTS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-    return( 
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Current amount</th>
-            </tr>
-            {data.currentUserAccounts.map(({ name, currentAmount }) => (
-                <tr>
-                    <td>{name}</td>
-                    <td>{currentAmount}</td>
-                </tr>
-            ))}
-        </table>
-    );
+  return( 
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Current amount</th>
+      </tr>
+      {data.currentUserAccounts.map(({ name, currentAmount }) => (
+        <tr>
+          <td>{name}</td>
+          <td>{currentAmount}</td>
+        </tr>
+      ))}
+    </table>
+  );
 }
 
--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -1,53 +1,53 @@
 import { useQuery, gql } from '@apollo/client';
 
 const GET_TRANSACTIONS = gql`
-    {
-        currentUserTransactions {
-        amount
-        transferDate
-        debitAccount {
-            name
-            user {
-            name
-            }
+  {
+    currentUserTransactions {
+      amount
+      transferDate
+      debitAccount {
+        name
+        user {
+          name
         }
-        creditAccount {
-            name
-            user {
-            name
-            }
+      }
+      creditAccount {
+        name
+        user {
+          name
         }
-        }
+      }
     }
+  }
 `;
 
 const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric"}
-    return new Date(dateString).toLocaleDateString(undefined, options)
+  const options = { year: "numeric", month: "long", day: "numeric"}
+  return new Date(dateString).toLocaleDateString(undefined, options)
 }
 
 export default function Transactions() {        
-    const { loading, error, data } = useQuery(GET_TRANSACTIONS);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+  const { loading, error, data } = useQuery(GET_TRANSACTIONS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-    return( 
-        <table>
-            <tr>
-                <th>Debit account</th>
-                <th>Credit account</th>
-                <th>Transfer date</th>
-                <th>Amount</th>
-            </tr>
-            {data.currentUserTransactions.map(({ amount, transferDate, debitAccount, creditAccount }) => (
-                <tr>
-                    <td>{debitAccount.user.name}'s {debitAccount.name}</td>
-                    <td>{creditAccount.user.name}'s {creditAccount.name}</td>
-                    <td>{formatDate(transferDate)}</td>
-                    <td>{amount}</td>
-                </tr>
-            ))}
-        </table>
-    );
+  return( 
+    <table>
+      <tr>
+        <th>Debit account</th>
+        <th>Credit account</th>
+        <th>Transfer date</th>
+        <th>Amount</th>
+      </tr>
+      {data.currentUserTransactions.map(({ amount, transferDate, debitAccount, creditAccount }) => (
+          <tr>
+            <td>{debitAccount.user.name}'s {debitAccount.name}</td>
+            <td>{creditAccount.user.name}'s {creditAccount.name}</td>
+            <td>{formatDate(transferDate)}</td>
+            <td>{amount}</td>
+          </tr>
+      ))}
+    </table>
+  );
 }
 
